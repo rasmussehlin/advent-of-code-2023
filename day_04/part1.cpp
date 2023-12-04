93e605e7d3a5babb6722bd6a4a71c2bd41cf34af
Rasmus Sehlin <rasmus@sehl.in>
2023-12-04 07:25:13 +0100

    Day 4 part 1 done

--- day_04/part1.cpp
+++ day_04/part1.cpp
@@ -3,6 +3,8 @@
 #include <string>
 using namespace std;
 
+int getPointsOfCard(string line);
+
 int main()
 {
     // Code goes here
@@ -15,7 +17,7 @@ int main()
     {
         while (getline(myfile, line))
         {
-            // Do stuff...
+            sum += getPointsOfCard(line);
         }
     }
 
@@ -25,4 +27,56 @@ int main()
     myfile.close();
 
     return 0;
+}
+
+int getPointsOfCard(string line)
+{
+    if (line.compare("") == 0)
+    {
+        return 0;
+    }
+
+    int colon = line.find(':');
+    int splitpoint = line.find('|');
+    string winningNumbers = line.substr(colon + 1, splitpoint - colon - 1);
+    string scratchedNumbers = line.substr(splitpoint + 1, line.length() - splitpoint - 1);
+    cout << line.substr(line.find(' '), colon - line.find(' ')) << '\n';
+    cout << winningNumbers << '\n';
+    cout << scratchedNumbers << '\n';
+
+    int index = 1;
+    while (line[index] == ' ') { index++; }
+    int uBound = scratchedNumbers.find(' ', index);
+
+    int numberOfWins = 0;
+    while (index < scratchedNumbers.length())
+    {
+        int wIndex = 1;
+        while (winningNumbers[wIndex] == ' ') { wIndex++; }
+        int wUBound = winningNumbers.find(' ', 1);
+
+        while (wIndex < winningNumbers.length())
+        {
+            string scratchedNumber = scratchedNumbers.substr(index, uBound - index);
+            string winningNumber = winningNumbers.substr(wIndex, wUBound - wIndex);
+            if (scratchedNumber.compare(winningNumber) == 0)
+            {
+                numberOfWins++;
+            }
+            wIndex = wUBound;
+            while (winningNumbers[wIndex] == ' ') { wIndex++; }
+            wUBound = winningNumbers.find(' ', wIndex);
+        }
+        index = uBound;
+        while (scratchedNumbers[index] == ' ') { index++; }
+        uBound = scratchedNumbers.find(' ', index);
+    }
+
+    int result = numberOfWins > 0 ? 1 : 0;
+    for (int i = 0; i < numberOfWins - 1; i++)
+    {
+        result = result << 1;
+    }
+
+    return result;
 }
