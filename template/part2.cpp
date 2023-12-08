b449ef9f80f0ce4d5d6e9d4c68ca5d5a4e181374
Rasmus Sehlin <rasmus@sehl.in>
2023-12-08 07:56:09 +0100

    Update template with includes and args parameter

--- template/part2.cpp
+++ template/part2.cpp
@@ -1,13 +1,21 @@
 #include <iostream>
 #include <fstream>
 #include <string>
+#include "../AoCUtil.cpp"
+#include <boost/cstdint.hpp>
 using namespace std;
 
-int main()
+int main(int argc, char *argv[])
 {
+    if (argc != 2)
+    {
+        cout << "Needs to be exactly one argument. Exiting...\n";
+        return 0;
+    }
+
     // Code goes here
     ifstream myfile;
-    myfile.open("input");
+    myfile.open(argv[1]);
     string line;
     int sum = 0;
 
